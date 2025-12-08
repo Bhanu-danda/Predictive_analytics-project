3eb96ebfd26e6494f494c438f7e4077167b90a18
bhanu prasad <dandabhanu116@gmail.com>
2025-12-08 19:27:30 +0530

    few changes

--- ML_project.py
+++ ML_project.py
@@ -17,5 +17,31 @@ print(df.isnull().sum())
 
 df=df[['reviews.text','reviews.rating']]
 print(df)
+print("after new dataset")
+df = df.dropna(subset=['reviews.text', 'reviews.rating'])
+
+print("hello i am from duplicate",df.duplicated().sum())
+df = df.drop_duplicates()
+print("hello iam from duplicate2",df.duplicated().sum())
+
+# rating 4 or 5 → Positive  
+# rating 3 → Neutral  
+# rating 1 or 2 → Negative
+
+def rating_to_sentiment(r):
+    if r >= 4:
+        return "positive"
+    elif r == 3:
+        return "neutral"
+    else:
+        return "negative"
+
+df["sentiment"] = df["reviews.rating"].apply(rating_to_sentiment)
+
+print("breakpoint - 1")
+print(df.shape)
+print(df.isnull().sum())
+print(df.head())
+print(df.tail()) 
 
 
