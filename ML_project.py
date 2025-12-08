4f354201c1a920a87c6414cb0ad906a636550d37
bhanu prasad <dandabhanu116@gmail.com>
2025-12-08 12:28:41 +0530

    your message

--- ML_project.py
+++ ML_project.py
@@ -1,8 +1,15 @@
 import numpy as np
 import pandas as pd
-import matplotlib.pylot as plt
+import matplotlib.pyplot as plt
 import seaborn as ans
 
 from sklearn.linear_model import LinearRegression 
 from sklearn.linear_model import LogisticRegression
+
+df=pd.read_csv("C:/Users/BHANU PRASAD/Downloads/New folder/Datafiniti_Hotel_Reviews.csv")
+print(df.shape)
+print(df.info())
+print(df.head())
+print(df.describe(include='all')) 
+print(df.isnull().sum())
 
